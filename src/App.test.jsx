d6b868334cc2d8e97a0b73f57b417c2a873ebb04
Joep Meindertsma <joep@argu.co>
2021-03-13 14:24:01 +0100

    Fix test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,7 +6,7 @@ import App from './App';
 describe('<App>', () => {
   it('renders resource after clicking on fetch', async () => {
     const { findByText, getByText } = render(<App />);
-    fireEvent.click(getByText('atomicdata.dev'));
+    fireEvent.click(getByText('List of Classes'));
     // const linkElement = await findByText(/recommends/i);
     // expect(document.body.contains(linkElement));
     // fireEvent.click(getByTitle('Create a new Resource"'));
