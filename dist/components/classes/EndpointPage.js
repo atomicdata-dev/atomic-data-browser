f3c06f629e31836e85a2032d7886d2da4ce34594
joepio <joepmeindertsma@gmail.com>
2021-03-13 14:41:44 +0000

    Deploying to gh-pages from @ joepio/atomic-data-browser@99df8b90c3a781114ac8ee12b055d396c12492a6 🚀

--- dist/components/classes/EndpointPage.js
+++ dist/components/classes/EndpointPage.js
@@ -6,22 +6,25 @@ import Markdown from "../datatypes/Markdown.js";
 import FieldLabeled from "../forms/Field.js";
 import {Button} from "../Button.js";
 import {openURL} from "../../helpers/navigation.js";
+import {useHistory} from "../../pkg/react-router-dom.js";
 function EndpointPage({resource}) {
   const title = useTitle(resource);
   const [description] = useString(resource, properties.description);
   const [parameters] = useArray(resource, properties.endpoint.parameters);
   const [virtualResource] = useResource(null);
   const store = useStore();
-  function constructSubject() {
+  const history = useHistory();
+  async function constructSubject() {
     const url = new URL(resource.getSubject());
-    parameters.map(async (propUrl) => {
+    await Promise.all(parameters.map(async (propUrl) => {
       const val = virtualResource.get(propUrl);
       if (val != null) {
         const fullprop = await store.getProperty(propUrl);
+        console.log("prop", fullprop);
         url.searchParams.set(fullprop.shortname, val.toString());
       }
-    });
-    openURL(url.href);
+    }));
+    history.push(openURL(url.href));
   }
   return /* @__PURE__ */ React.createElement(ContainerNarrow, {
     about: resource.getSubject()
