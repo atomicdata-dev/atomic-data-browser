ec3107a05cdc2c356d164b550081979b2a8bed20
joepio <joepmeindertsma@gmail.com>
2021-03-13 13:31:52 +0000

    Deploying to gh-pages from @ joepio/atomic-data-browser@d6b868334cc2d8e97a0b73f57b417c2a873ebb04 🚀

--- dist/routes/WelcomeRoute.js
+++ dist/routes/WelcomeRoute.js
@@ -4,23 +4,27 @@ import {ContainerNarrow} from "../components/Containers.js";
 import AtomicLink, {LinkView} from "../components/Link.js";
 export const Welcome = () => {
   const history = useHistory();
-  return /* @__PURE__ */ React.createElement(ContainerNarrow, null, /* @__PURE__ */ React.createElement("h1", null, "Atomic Data"), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("a", {
-    href: "https://atomicdata.dev"
-  }, "Atomic Data"), " is a new set of standards designed to make it easier to share, create and model linked data. Check out", " ", /* @__PURE__ */ React.createElement("b", null, /* @__PURE__ */ React.createElement("a", {
+  return /* @__PURE__ */ React.createElement(ContainerNarrow, null, /* @__PURE__ */ React.createElement("h1", null, "Atomic Data"), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("em", null, "The easiest way to ", /* @__PURE__ */ React.createElement("b", null, "create"), ", ", /* @__PURE__ */ React.createElement("b", null, "share"), " and ", /* @__PURE__ */ React.createElement("b", null, "model"), " linked data.")), /* @__PURE__ */ React.createElement("p", null, "Atomic Data is a proposed standard for modeling and exchanging linked data. It uses links to connect pieces of data, and therefore makes it easier to connect datasets to each other - even when these datasets exist on separate machines. It aims to help realize a more decentralized internet that encourages data ownership and interoperability."), /* @__PURE__ */ React.createElement("p", null, "Atomic Data is especially suitable for knowledge graphs, distributed datasets, semantic data, p2p applications, decentralized apps, and data that is meant to be shared. It is designed to be highly extensible, easy to use, and to make the process of domain specific standardization as simple as possible. Check out", " ", /* @__PURE__ */ React.createElement("b", null, /* @__PURE__ */ React.createElement("a", {
     href: "https://docs.atomicdata.dev/"
-  }, "the docs")), " ", "for more information about Atomic Data."), /* @__PURE__ */ React.createElement("p", null, "This app is for viewing, editing and creating Atomic Data. It's free and open source on", " ", /* @__PURE__ */ React.createElement("a", {
+  }, "the docs")), " ", "for more information about Atomic Data."), /* @__PURE__ */ React.createElement("h2", null, "About this app"), /* @__PURE__ */ React.createElement("p", null, "You're looking at ", /* @__PURE__ */ React.createElement("a", {
     href: "https://github.com/joepio/atomic-data-browser"
-  }, "github"), ". Please add an issue if you encouter problems or have a feature request. Expect bugs and issues, because this stuff is pretty beta."), /* @__PURE__ */ React.createElement("p", null, "You can edit app settings, such as current user and theme color at", " ", /* @__PURE__ */ React.createElement(LinkView, {
+  }, "atomic-data-browser"), ", an open-source client for viewing and editing data. Please add an issue if you encouter problems or have a feature request. Expect bugs and issues, because this stuff is pretty beta."), /* @__PURE__ */ React.createElement("p", null, "The back-end of this app is ", /* @__PURE__ */ React.createElement("a", {
+    href: "https://github.com/joepio/atomic"
+  }, "atomic-server"), ", which you can think of as an open source, web-native database."), /* @__PURE__ */ React.createElement("p", null, "You can edit app settings, such as current user and theme color at", " ", /* @__PURE__ */ React.createElement(LinkView, {
     onClick: () => history.push("/settings")
   }, "/settings"), "."), /* @__PURE__ */ React.createElement("p", null, "Check out the keyboard shortcuts at ", /* @__PURE__ */ React.createElement(LinkView, {
     onClick: () => history.push("/shortcuts")
-  }, "/shortcuts"), "."), /* @__PURE__ */ React.createElement("p", null, "Some ", /* @__PURE__ */ React.createElement("a", {
-    href: "https://github.com/joepio/atomic"
-  }, "atomic-servers"), " to visit with this browser:"), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(AtomicLink, {
+  }, "/shortcuts"), "."), /* @__PURE__ */ React.createElement("h2", null, "Things to visit"), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(AtomicLink, {
     url: "https://atomicdata.dev/collections/collection"
-  }, "atomicdata.dev"))), /* @__PURE__ */ React.createElement("p", null, "Or run your own server..."), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("code", null, "docker run -p 80:80 -p 443:443 -v atomic-storage:/atomic-storage joepmeneer/atomic-server")), /* @__PURE__ */ React.createElement("p", null, "...and visit ", /* @__PURE__ */ React.createElement(AtomicLink, {
+  }, "List of lists")), /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(AtomicLink, {
+    url: "https://atomicdata.dev/collections/class"
+  }, "List of Classes")), /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(AtomicLink, {
+    url: "https://atomicdata.dev/collections/property"
+  }, "List of Properties"))), /* @__PURE__ */ React.createElement("h2", null, "Run your own server"), /* @__PURE__ */ React.createElement("p", null, "The easiest way to run an ", /* @__PURE__ */ React.createElement("a", {
+    href: "https://github.com/joepio/atomic"
+  }, "atomic-server"), " is by using Docker:"), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("code", null, "docker run -p 80:80 -p 443:443 -v atomic-storage:/atomic-storage joepmeneer/atomic-server")), /* @__PURE__ */ React.createElement("p", null, "...and visit ", /* @__PURE__ */ React.createElement(AtomicLink, {
     url: "http://localhost/collections/collection"
-  }, "localhost"), "."), /* @__PURE__ */ React.createElement("p", null, "If you have any questions, feel free to join our ", /* @__PURE__ */ React.createElement("a", {
+  }, "localhost"), "."), /* @__PURE__ */ React.createElement("h2", null, "Join the community"), /* @__PURE__ */ React.createElement("p", null, "Atomic Data is open and fully powered by volunteers. We're looking for people who want to help discuss various design challenges and work on implmenentations. If you have any questions, or want to help out, feel free to join our", " ", /* @__PURE__ */ React.createElement("a", {
     href: "https://discord.gg/a72Rv2P"
   }, "Discord"), "!"));
 };
