624912bff69c4f744c07b0faae8f0df27fd91023
joepio <joepmeindertsma@gmail.com>
2021-03-14 19:09:10 +0000

    Deploying to gh-pages from @ joepio/atomic-data-browser@f7ab75f117cf74a2dd960a6831d75bc0626e7326 🚀

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,11 @@ import.meta.env = __SNOWPACK_ENV__;
 
 import React from "./pkg/react.js";
 import ReactDOM from "./pkg/react-dom.js";
+import registerServiceWorker from "./serviceWorker.js";
 import App from "./App.js";
 export const Root = () => /* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null));
 ReactDOM.render(/* @__PURE__ */ React.createElement(Root, null), document.getElementById("root"));
 if (undefined /* [snowpack] import.meta.hot */ ) {
   undefined /* [snowpack] import.meta.hot */ .accept();
 }
+registerServiceWorker();
