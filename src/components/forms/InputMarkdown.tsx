69c6b23c250731318c2d4c42666223274512320a
Joep Meindertsma <joep@ontola.io>
2021-04-28 22:51:47 +0200

    Yamde styling

--- src/components/forms/InputMarkdown.tsx
+++ src/components/forms/InputMarkdown.tsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
 import { useString } from '../../atomic-react/hooks';
 import { InputProps } from './ResourceField';
-import { ErrMessage, InputWrapper, TextAreaStyled } from './InputStyles';
+import { ErrMessage, InputWrapper } from './InputStyles';
 import Yamde from 'yamde';
 import { useSettings } from '../../helpers/AppSettings';
 import styled from 'styled-components';
 
-export default function InputMarkdown({ resource, property, required, autoFocus }: InputProps): JSX.Element {
+export default function InputMarkdown({ resource, property }: InputProps): JSX.Element {
   const [value, setVale] = useString(resource, property.subject);
   const [err, setErr] = useState<Error>(null);
   const { darkMode } = useSettings();
 
-  // function handleUpdate(e) {
-  //   const newval = e.target.value;
-  //   setVale(newval, setErr);
-  // }
-
   return (
     <>
       <InputWrapper>
-        <YamdeS value={value} handler={e => setVale(e, setErr)} theme={darkMode ? 'dark' : 'light'} />
+        <YamdeStyling>
+          <Yamde value={value} handler={e => setVale(e, setErr)} theme={darkMode ? 'dark' : 'light'} />
+        </YamdeStyling>
         {/* <TextAreaStyled rows={3} value={value == null ? '' : value} onChange={handleUpdate} required={required} autoFocus={autoFocus} /> */}
       </InputWrapper>
       {value !== '' && err && <ErrMessage>{err.message}</ErrMessage>}
@@ -28,7 +25,8 @@ export default function InputMarkdown({ resource, property, required, autoFocus 
   );
 }
 
-const YamdeS = styled(Yamde)`
-  margin: 0;
-  background: red;
+const YamdeStyling = styled.div`
+  .yamde-0-2-1 {
+    margin: 0;
+  }
 `;
