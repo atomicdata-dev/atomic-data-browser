20df5d0b3f8cc122a15ed26cd36d75629fd0cd69
Joep Meindertsma <joep@ontola.io>
2021-04-29 16:22:26 +0200

    Fix search params in LocalRoute

--- src/routes/LocalRoute.tsx
+++ src/routes/LocalRoute.tsx
@@ -4,10 +4,10 @@ import ResourcePage from '../components/ResourcePage';
 
 /** Show a resource where the domain matches the current domain */
 function Local(): JSX.Element {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   // The key makes sure the component re-renders when it changes
-  return <ResourcePage key={pathname} subject={window.location.origin + pathname} />;
+  return <ResourcePage key={pathname} subject={window.location.origin + pathname + search} />;
 }
 
 export default Local;
