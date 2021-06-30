9ccf567ca49603a730ccee3b7ad9db66fff56a90
joepio <joepmeindertsma@gmail.com>
2021-06-30 22:14:09 +0000

    Deploying to gh-pages from @ joepio/atomic-data-browser@31892e51059d35186b1712e6ac97f36dba4bc182 🚀

--- dist/views/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import * as React from "../pkg/react.js";
-import {ContainerNarrow} from "../components/Containers.js";
-import {ErrorLook} from "../components/ResourceInline.js";
-function ErrorPage({resource, children}) {
-  return /* @__PURE__ */ React.createElement(ContainerNarrow, {
-    resource: resource.getSubject()
-  }, /* @__PURE__ */ React.createElement(ErrorLook, null, children));
-}
-export default ErrorPage;
