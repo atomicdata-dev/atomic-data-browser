f00b4fd667ca1609c0d2e525ab22e4a8481b3e37
joepio <joepio@users.noreply.github.com>
2021-07-28 16:45:51 +0000

    Deploying to gh-pages from @ joepio/atomic-data-browser@28e410b1da775057f9e632aad841d76eaaf564c2 🚀

--- dist/helpers/loadDefaultStore.js
+++
@@ -1,8 +0,0 @@
-import {parseJsonADArray} from "../link/lib/src/index.js";
-import default_store from "../default_store.json";
-export function loadDefaultStore(store) {
-  const resources = parseJsonADArray(default_store);
-  for (const resource of resources) {
-    store.addResource(resource);
-  }
-}
