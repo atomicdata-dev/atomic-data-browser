4511a473d1e8f83fdab78af313c4fa0bbf8407ca
Joep Meindertsma <joep@ontola.io>
2021-08-30 15:10:53 +0200

    #85 Error handling in ValueComp

--- data-browser/src/components/ValueComp.tsx
+++ data-browser/src/components/ValueComp.tsx
@@ -5,6 +5,7 @@ import DateTime from './datatypes/DateTime';
 import Markdown from './datatypes/Markdown';
 import Nestedresource from './datatypes/NestedResource';
 import ResourceArray from './datatypes/ResourceArray';
+import { ErrMessage } from './forms/InputStyles';
 
 type Props = {
   value: Value;
@@ -14,22 +15,30 @@ type Props = {
 
 /** Renders a value in a fitting way, depending on its DataType */
 function ValueComp({ value, datatype, noMargin }: Props): JSX.Element {
-  switch (datatype) {
-    case Datatype.ATOMIC_URL: {
-      const resource = value.toResource();
-      if (typeof resource == 'string') {
-        return <ResourceInline subject={resource} />;
+  try {
+    switch (datatype) {
+      case Datatype.ATOMIC_URL: {
+        const resource = value.toResource();
+        if (typeof resource == 'string') {
+          return <ResourceInline subject={resource} />;
+        }
+        return <Nestedresource resource={resource} />;
       }
-      return <Nestedresource resource={resource} />;
+      case (Datatype.DATE, Datatype.TIMESTAMP):
+        return <DateTime date={value.toDate()} />;
+      case Datatype.MARKDOWN:
+        return <Markdown text={value.toString()} noMargin={noMargin} />;
+      case Datatype.RESOURCEARRAY:
+        return <ResourceArray subjects={value.toArray()} />;
+      default:
+        return <div>{value.toString()}</div>;
     }
-    case (Datatype.DATE, Datatype.TIMESTAMP):
-      return <DateTime date={value.toDate()} />;
-    case Datatype.MARKDOWN:
-      return <Markdown text={value.toString()} noMargin={noMargin} />;
-    case Datatype.RESOURCEARRAY:
-      return <ResourceArray subjects={value.toArray()} />;
-    default:
-      return <div>{value.toString()}</div>;
+  } catch (e) {
+    return (
+      <ErrMessage>
+        {e.message} original value: {value.toString()}
+      </ErrMessage>
+    );
   }
 }
 
