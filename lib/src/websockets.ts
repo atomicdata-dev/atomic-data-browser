e2b055f9f977cb5755a5624b221c5fb25d7acfb4
Joep Meindertsma <joep@ontola.io>
2021-09-21 21:25:55 +0200

    Remove logs from websockets #80

--- lib/src/websockets.ts
+++ lib/src/websockets.ts
@@ -3,7 +3,6 @@ import { Store } from './store';
 
 /** Opens a Websocket Connection at `/ws` for the current Drive */
 export function startWebsocket(store: Store): WebSocket {
-  console.log('starting websocket with', store.getBaseUrl());
   const wsURL = new URL(store.getBaseUrl());
   wsURL.protocol = 'ws';
   wsURL.pathname = '/ws';
@@ -12,7 +11,6 @@ export function startWebsocket(store: Store): WebSocket {
   client.onmessage = (ev: MessageEvent) => handleMessage(ev, store);
   client.onerror = handleError;
   client.onclose = handleClose;
-  console.log('starting websocket...', client);
   return client;
 }
 
@@ -24,7 +22,6 @@ function handleOpen(store: Store) {
 }
 
 function handleMessage(ev: MessageEvent, store: Store) {
-  console.log('message', ev.data);
   if (ev.data.startsWith('COMMIT ')) {
     const commit = ev.data.slice(7);
     parseAndApply(commit, store);
@@ -37,6 +34,4 @@ function handleError(ev: Event) {
   console.log('websocket error:', ev);
 }
 
-function handleClose(ev: CloseEvent) {
-  console.log('websocket close:', ev);
-}
+function handleClose(ev: CloseEvent) { }
