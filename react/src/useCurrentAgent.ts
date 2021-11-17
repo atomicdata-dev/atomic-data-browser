fef889ccde150727389d8372cef107e65c57d63f
Joep Meindertsma <joep@ontola.io>
2021-11-17 20:56:45 +0100

    Fix two errors

--- react/src/useCurrentAgent.ts
+++ react/src/useCurrentAgent.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Agent } from '@tomic/lib';
 import { useStore } from './hooks';
 import { useLocalStorage } from './useLocalStorage';
