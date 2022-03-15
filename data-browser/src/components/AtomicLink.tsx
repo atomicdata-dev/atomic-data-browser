f26d1174861835d9a56e00d320e591bca8eeb35d
Joep Meindertsma <joep@ontola.io>
2022-03-15 09:23:57 +0100

    #149 WIP menu links href

--- data-browser/src/components/AtomicLink.tsx
+++ data-browser/src/components/AtomicLink.tsx
@@ -7,7 +7,7 @@ import { FaExternalLinkAlt } from 'react-icons/fa';
 import { ErrorLook } from '../views/ResourceInline';
 import { isRunningInTauri } from '../helpers/tauri';
 
-type Props = {
+export interface AtomicLinkProps {
   children?: ReactNode;
   /** An http URL to an Atomic Data resource, opened in this app and fetched as JSON-AD */
   subject?: string;
@@ -16,16 +16,19 @@ type Props = {
   /** A path string, e.g. /new, opened using the internal router */
   path?: string;
   untabbable?: boolean;
-};
+  /** Minimal styling applied */
+  clean?: boolean;
+}
 
 /** Renders a link. Either a subject or a href is required */
 export function AtomicLink({
   children,
+  clean,
   subject,
   path,
   href,
   untabbable,
-}: Props): JSX.Element {
+}: AtomicLinkProps): JSX.Element {
   const [currentUrl] = useCurrentSubject();
   const history = useHistory();
 
@@ -59,6 +62,7 @@ export function AtomicLink({
 
   return (
     <LinkView
+      clean={clean}
       about={subject}
       onClick={handleClick}
       href={subject ? subject : href}
@@ -76,6 +80,8 @@ export function AtomicLink({
 
 type Proppies = {
   disabled?: boolean;
+  /** Minimal styling applied */
+  clean?: boolean;
 };
 
 /** Look clickable, should be used for opening things only - not interactions. */
@@ -93,7 +99,7 @@ export const LinkView = styled.a<Proppies>`
 
   &:hover {
     color: ${props => props.theme.colors.mainLight};
-    text-decoration: underline;
+    text-decoration: ${p => (p.clean ? 'none' : 'underline')};
   }
   &:active {
     color: ${props => props.theme.colors.mainDark};
