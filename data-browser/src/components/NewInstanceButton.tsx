42c73e77e4f3a7b01b85d7a88c8e545f413da8b0
Polle Pas <polle@argu.co>
2022-07-26 11:57:56 +0200

    #187 Add view for bookmarks

---
+++ data-browser/src/components/NewInstanceButton.tsx
@@ -0,0 +1,101 @@
+import React, { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { newURL, openURL } from '../helpers/navigation';
+import {
+  useCurrentAgent,
+  useResource,
+  useStore,
+  useString,
+  useTitle,
+} from '@tomic/react';
+import { Button } from './Button';
+import { FaPlus } from 'react-icons/fa';
+import { classes, properties, Resource } from '@tomic/lib';
+import toast from 'react-hot-toast';
+import { paths } from '../routes/paths';
+
+type NewIntanceButtonProps = {
+  klass: string;
+  subtle?: boolean;
+  icon?: boolean;
+  /** ID of the parent Resource, which will be passed to the form */
+  parent?: string;
+  children?: ReactNode;
+};
+
+/** A button for creating a new instance of some thing */
+function NewIntanceButton({
+  klass,
+  subtle,
+  icon,
+  parent,
+  children,
+}: NewIntanceButtonProps): JSX.Element {
+  const resource = useResource(klass);
+  const title = useTitle(resource);
+  const navigate = useNavigate();
+  const store = useStore();
+  const [agent] = useCurrentAgent();
+  const [shortname] = useString(resource, properties.shortname);
+
+  if (parent == undefined) {
+    // TODO: This is not a permanent solution
+    parent = store.getAgent()?.subject;
+  }
+
+  let onClick = async function onClick() {
+    // Opens an `Edit` form with the class and a decent subject name
+    navigate(newURL(klass, parent, store.createSubject(shortname)));
+  };
+
+  switch (klass) {
+    case classes.chatRoom: {
+      onClick = async () => {
+        const subject = store.createSubject('chatRoom');
+        const resource = new Resource(subject, true);
+        await Promise.all([
+          resource.set(properties.name, 'New ChatRoom', store),
+          resource.set(properties.isA, [classes.chatRoom], store),
+          resource.set(properties.parent, parent, store),
+        ]);
+        await resource.save(store);
+        navigate(openURL(subject));
+        toast.success('ChatRoom created');
+      };
+      break;
+    }
+    case classes.document: {
+      onClick = async () => {
+        const subject = store.createSubject('documents');
+        const resource = new Resource(subject, true);
+        await Promise.all([
+          resource.set(properties.isA, [classes.document], store),
+          resource.set(properties.parent, parent, store),
+        ]);
+        await resource.save(store);
+        navigate(openURL(subject));
+        toast.success('Document created');
+      };
+    }
+  }
+
+  if (!agent) {
+    onClick = async () => {
+      toast.error('You need to be logged in to create new things');
+      navigate(paths.agentSettings);
+    };
+  }
+
+  return (
+    <Button
+      onClick={onClick}
+      subtle={subtle}
+      title={agent ? `Create a new ${title}` : 'No User set - first sign in'}
+    >
+      {icon ? <FaPlus /> : `new ${title}`}
+      {children}
+    </Button>
+  );
+}
+
+export default NewIntanceButton;
