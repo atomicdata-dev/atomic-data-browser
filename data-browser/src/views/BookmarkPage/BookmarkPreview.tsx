64eb8cc8733d33d8346465991292b5b6900dcbd9
Polle Pas <polle@argu.co>
2022-08-02 16:53:21 +0200

    #187 Streamline bookmark creation

--- data-browser/src/views/BookmarkPage/BookmarkPreview.tsx
+++ data-browser/src/views/BookmarkPage/BookmarkPreview.tsx
@@ -1,81 +1,21 @@
-import {
-  Resource,
-  Store,
-  urls,
-  useDebounce,
-  useStore,
-  useString,
-} from '@tomic/react';
-//@ts-ignore
-import React, { useEffect, useState, useTransition } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ContainerNarrow } from '../../components/Containers.jsx';
 import Markdown from '../../components/datatypes/Markdown.jsx';
 
 export interface BookmarkPreviewProps {
-  resource: Resource;
-}
-
-async function fetchBookmarkData(url: string, name: string, store: Store) {
-  const bookmarkRoute = new URL('/fetchbookmark', store.serverUrl);
-
-  const searchParams = new URLSearchParams({
-    name,
-    url,
-  });
-
-  bookmarkRoute.search = searchParams.toString();
-
-  const res = await fetch(bookmarkRoute, {
-    headers: {
-      Accept: 'application/json-ad',
-    },
-  });
-
-  return await res.json();
+  preview: string;
+  error?: boolean;
 }
 
 export function BookmarkPreview({
-  resource,
+  preview,
+  error,
 }: BookmarkPreviewProps): JSX.Element {
-  const store = useStore();
-  const [preview, setPreview] = useString(
-    resource,
-    urls.properties.bookmark.preview,
-  );
-  const [url] = useString(resource, urls.properties.bookmark.url);
-  const [name, setName] = useString(resource, urls.properties.name);
-  const [hasError, setHasError] = useState(false);
-
-  const debouncedURL: string = useDebounce(url, 500);
-  const [_, startTransition] = useTransition();
-
-  useEffect(() => {
-    console.log('fetching');
-    try {
-      new URL(debouncedURL);
-    } catch (e) {
-      return;
-    }
-
-    startTransition(() => {
-      fetchBookmarkData(debouncedURL, name, store)
-        .then(res => {
-          setPreview(res.preview);
-          setName(res.name);
-          setHasError(false);
-          resource.save(store);
-        })
-        .catch(err => {
-          console.error(err);
-          setHasError(true);
-        });
-    });
-  }, [debouncedURL]);
-
-  if (hasError) {
+  if (error) {
     return <ErrorPage />;
   }
+
   return (
     <StyledContainerNarrow>
       <Markdown renderGFM text={preview} />
