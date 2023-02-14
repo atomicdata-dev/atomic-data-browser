19d069604505af3232dc5a18992e06251974b442
Joep Meindertsma <joep@ontola.io>
2023-02-14 23:39:55 +0100

    Improve error handler

--- data-browser/src/helpers/loggingHandlers.tsx
+++ data-browser/src/helpers/loggingHandlers.tsx
@@ -3,11 +3,11 @@ import BugsnagPluginReact, {
   BugsnagErrorBoundary,
 } from '@bugsnag/plugin-react';
 import React from 'react';
-import { toast } from 'react-hot-toast';
 import { isDev } from '../config';
 
 export function handleError(e: Error): void {
-  toast.error(e.message);
+  // We already toast in the `errorHandler`
+  // toast.error(e.message);
   console.error(e);
 
   if (!isDev) {
