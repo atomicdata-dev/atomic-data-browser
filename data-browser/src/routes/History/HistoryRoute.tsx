9dbdaaf55a39c8e1f6a8db067ad890cb4c2f4a5a
Polle Pas <polleps@gmail.com>
2023-06-09 13:39:34 +0200

    #39 Responsive views and nav buttons for history

--- data-browser/src/routes/History/HistoryRoute.tsx
+++ data-browser/src/routes/History/HistoryRoute.tsx
@@ -1,33 +1,24 @@
-import React, { useEffect, useId, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useResource, useStore, Version } from '@tomic/react';
 
 import { ContainerNarrow } from '../../components/Containers';
 import { useCurrentSubject } from '../../helpers/useCurrentSubject';
-import { Title } from '../../components/Title';
 import { ErrorLook } from '../../components/ErrorLook';
-import { ResourceCardDefault } from '../../views/Card/ResourceCard';
-import { Column } from '../../components/Row';
 import styled from 'styled-components';
-import { Button } from '../../components/Button';
 import { useVersions } from './useVersions';
-import { VersionButton } from './VersionButton';
-import { Card } from '../../components/Card';
-import { VersionTitle } from './VersionTitle';
-import {
-  dedupeVersions,
-  groupVersionsByMonth,
-  setResourceToVersion,
-} from './versionHelpers';
+import { groupVersionsByMonth, setResourceToVersion } from './versionHelpers';
 import { toast } from 'react-hot-toast';
 import { useNavigateWithTransition } from '../../hooks/useNavigateWithTransition';
 import { constructOpenURL } from '../../helpers/navigation';
+import { HistoryDesktopView } from './HistoryDesktopView';
+import { HistoryMobileView } from './HistoryMobileView';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
 /** Shows an activity log of previous versions */
 export function History(): JSX.Element {
   const store = useStore();
   const navigate = useNavigateWithTransition();
-  const scrollerTitleId = useId();
-
+  const isSmallScreen = useMediaQuery('(max-width: 500px)');
   const [subject] = useCurrentSubject();
   const resource = useResource(subject);
   const { versions, loading, error } = useVersions(resource);
@@ -35,10 +26,12 @@ export function History(): JSX.Element {
 
   const groupedVersions: {
     [key: string]: Version[];
-  } = useMemo(() => groupVersionsByMonth(dedupeVersions(versions)), [versions]);
+  } = useMemo(() => groupVersionsByMonth(versions), [versions]);
 
   useEffect(() => {
-    setSelectedVersion(versions[versions.length - 1]);
+    if (versions.length > 0) {
+      setSelectedVersion(versions[versions.length - 1]);
+    }
   }, [versions]);
 
   const setResourceToCurrentVersion = async () => {
@@ -48,6 +41,30 @@ export function History(): JSX.Element {
       navigate(constructOpenURL(subject));
     }
   };
+
+  const nextVersion = useCallback(() => {
+    const currentIndex = versions.findIndex(v => v === selectedVersion);
+
+    if (currentIndex === -1 || currentIndex === versions.length - 1) {
+      return;
+    }
+
+    setSelectedVersion(versions[currentIndex + 1]);
+  }, [versions, selectedVersion]);
+
+  const previousVersion = useCallback(() => {
+    const currentIndex = versions.findIndex(v => v === selectedVersion);
+
+    if (currentIndex === -1 || currentIndex === 0) {
+      return;
+    }
+
+    setSelectedVersion(versions[currentIndex - 1]);
+  }, [versions, selectedVersion]);
+
+  const ViewComp = isSmallScreen ? HistoryMobileView : HistoryDesktopView;
+
+  const isCurrentVersion = selectedVersion === versions[versions.length - 1];
 
   if (loading) {
     return <ContainerNarrow>Loading history of {subject}...</ContainerNarrow>;
@@ -63,40 +80,16 @@ export function History(): JSX.Element {
 
   return (
     <SplitView about={subject}>
-      <CurrentItem>
-        <Column fullHeight>
-          <Title resource={resource} prefix='History of' link />
-          {selectedVersion && selectedVersion?.resource && (
-            <>
-              <VersionTitle version={selectedVersion} />
-              <StyledCard>
-                <ResourceCardDefault resource={selectedVersion.resource} />
-              </StyledCard>
-              <Button onClick={setResourceToCurrentVersion}>
-                Make current version
-              </Button>
-            </>
-          )}
-        </Column>
-      </CurrentItem>
-      <ScrollerSection aria-labelledby={scrollerTitleId}>
-        <Column>
-          <ScrollerTitle id={scrollerTitleId}>Versions</ScrollerTitle>
-          {Object.entries(groupedVersions).map(([key, group]) => (
-            <React.Fragment key={key}>
-              <GroupHeading>{key}</GroupHeading>
-              {[...group].map(version => (
-                <VersionButton
-                  onClick={() => setSelectedVersion(version)}
-                  version={version}
-                  key={version.commit.id}
-                  selected={selectedVersion?.commit.id === version.commit.id}
-                />
-              ))}
-            </React.Fragment>
-          ))}
-        </Column>
-      </ScrollerSection>
+      <ViewComp
+        resource={resource}
+        groupedVersions={groupedVersions}
+        selectedVersion={selectedVersion}
+        isCurrentVersion={isCurrentVersion}
+        onNextVersion={nextVersion}
+        onPreviousVersion={previousVersion}
+        onSelectVersion={setSelectedVersion}
+        onVersionAccept={setResourceToCurrentVersion}
+      />
     </SplitView>
   );
 }
@@ -106,50 +99,12 @@ const SplitView = styled.main`
   /* Fills entire view on all devices */
   width: 100%;
   height: 100%;
-  height: calc(100vh - 5rem);
+  height: calc(100vh - 6rem);
   padding: ${p => p.theme.margin}rem;
   gap: ${p => p.theme.margin}rem;
-`;
 
-const StyledCard = styled(Card)`
-  flex: 1;
-  overflow: auto;
-`;
-
-const CurrentItem = styled.div`
-  flex: 1;
-`;
-
-const ScrollerTitle = styled.h2`
-  text-align: center;
-`;
-const ScrollerSection = styled.section`
-  overflow: auto;
-  /* flex: 1; */
-  border: 1px solid ${p => p.theme.colors.bg2};
-  border-radius: ${p => p.theme.radius};
-  background-color: ${p => p.theme.colors.bg};
-  padding: 1rem;
-  width: 20rem;
-`;
-
-const GroupHeading = styled.h3`
-  &::before {
-    content: '';
-    display: block;
-    height: 1px;
-    background-color: ${p => p.theme.colors.bg2};
+  /* Fix code blocks not shrinking causing page overflow. */
+  & code {
+    word-break: break-word;
   }
-  &::after {
-    content: '';
-    display: block;
-    height: 1px;
-    background-color: ${p => p.theme.colors.bg2};
-  }
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
-  align-items: center;
-  gap: 0.5rem;
-  text-align: center;
-  color: ${p => p.theme.colors.textLight};
 `;
